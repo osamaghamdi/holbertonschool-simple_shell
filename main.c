350caf5a31fbeec068cd1c0c4472834de50645cb
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-17 21:46:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the shell program.
- * @ac: Argument count (unused).
- * @av: Argument vector (unused).
+ * main - Entry point for the shell program
  *
- * Return: Always returns 1 (not standard, but placeholder for now).
+ * Return: 0 on success
  */
-
-int main(int ac, char **av)
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
+    char *line;
 
-char *line;
+    while (1)
+    {
+        line = get_line();
+        printf("%s\n", line);
+    }
 
-while (1)
-{
-
-line = get_line();
-printf("%s\n", line);
-
-}
-
-return (1);
+    return (0);
 }
