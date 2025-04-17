8532c5c8b3fe9343e7c7cbe6333172ada9334e38
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-17 21:59:00 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -1,40 +1,24 @@
 #include "shell.h"
-
-/**
- * parse_line - splits a line into tokens (arguments)
- * @line: the input line
- *
- * Return: array of strings (tokens)
- */
-char **parse_line(char *line)
-{
-int bufsize = 64, i = 0;
-char **tokens = malloc(bufsize * sizeof(char *));
-char *token;
-
-if (!tokens)
-{
-perror("malloc");
-exit(EXIT_FAILURE);
-}
-
-token = strtok(line, " \n\t");
-while (token)
-{
-tokens[i++] = token;
-if (i >= bufsize)
-{
-bufsize += 64;
-tokens = realloc(tokens, bufsize *sizeof(char *));
-if (!tokens)
-{
-perror("realloc");
-exit(EXIT_FAILURE);
-}
-}
-token = strtok(NULL, " \n\t");
-}
-tokens[i] = NULL;
-return (tokens);
-free(tokens);
-}
+ 
+ char *get_line(void)
+ {
+ 
+ char *buffer;
+ size_t buffer_size;
+ 
+ buffer = NULL;
+ 
+ printf("($)");
+ 
+ if (getline(&buffer, &buffer_size, stdin) == -1)
+ {
+ buffer = NULL;
+ if (feof(stdin))
+ printf("[EOF]");
+ else
+ printf("Get line failed");
+ 
+ }
+ 
+ return (buffer);
+ }
