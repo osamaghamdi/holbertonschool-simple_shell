2d235067ec890368664415c481ad20daff3f2f7e
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-17 21:50:46 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -4,24 +4,24 @@
  * get_line - Reads a line of input from stdin.
  *
  * Return: A pointer to the buffer containing the input string,
- *         or NULL if an error occurs or EOF is reached.
+ * or NULL if an error occurs or EOF is reached.
  */
 char *get_line(void)
 {
-	char *buffer;
-	size_t buffer_size;
+    char *buffer;
+    size_t buffer_size;
 
-	buffer = NULL;
-	printf("($)");
+    buffer = NULL;
+    printf("($)");
 
-	if (getline(&buffer, &buffer_size, stdin) == -1)
-	{
-		buffer = NULL;
-		if (feof(stdin))
-			printf("[EOF]");
-		else
-			printf("Get line failed");
-	}
+    if (getline(&buffer, &buffer_size, stdin) == -1)
+    {
+        buffer = NULL;
+        if (feof(stdin))
+            printf("[EOF]");
+        else
+            printf("Get line failed");
+    }
 
-	return (buffer);
+    return (buffer);
 }
