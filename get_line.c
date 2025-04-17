63f44d6b6d4a3877162fc0e3665b4781eed56463
osama <ghamdios235@gmail.com>
2025-04-17 09:11:44 +0000

    func to get the line

---
+++ get_line.c
@@ -0,0 +1,24 @@
+#include "cell.h"
+
+char *get_line(void)
+{
+
+char *buffer;
+size_t buffer_size;
+
+buffer = NULL;
+
+printf("($)");
+
+if (getline(&buffer, &buffer_size, stdin) == -1)
+{
+buffer = NULL;
+if (feof(stdin))
+printf("[EOF]");
+else
+printf("Get line failed");
+
+}
+
+return buffer;
+}
