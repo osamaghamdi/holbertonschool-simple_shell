f49a9154c054067edea72402e130472fe29ea6a7
Your Name <you@example.com>
2025-04-17 18:17:30 +0000

    edit file get line

--- get_line.c
+++ get_line.c
@@ -10,22 +10,23 @@
 char *get_line(void)
 {
 
-char *buffer;
-size_t buffer_size;
+	char *buffer;
+	size_t buffer_size;
 
-buffer = NULL;
+	buffer = NULL;
 
-printf("($)");
+	printf("($)");
 
-if (getline(&buffer, &buffer_size, stdin) == -1)
-{
-buffer = NULL;
-if (feof(stdin))
-printf("[EOF]");
-else
-printf("Get line failed");
+	if (getline(&buffer, &buffer_size, stdin) == -1)
+	{
+		buffer = NULL;
+	if (feof(stdin))
+		printf("[EOF]");
+	else
+		printf("Get line failed");
 
-}
+	}
 
-return (buffer);
+	return (buffer);
+
 }
